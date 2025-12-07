bb4a931f69b3e4e8864bb8d5d1e4f2a5aa15aeac
Javith-Farvez <mohammedjavithfarvez007@gmail.com>
2025-12-08 03:09:39 +0530

    Move files to Final Project/Frontend/

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,12 @@
 # models/__init__.py
 
-from .models import (
-    problem1_logic,
-    problem2_logic,
-    problem3_logic,
-    problem4_logic,
-    problem5_logic
-)
+ if st.session_state.selected_page == "league_winner":
+    league_winner_prediction()
+elif st.session_state.selected_page == "points_prediction":
+    total_points_prediction_page()
+elif st.session_state.selected_page == "goals_prediction":
+    goals_prediction_page()
+elif st.session_state.selected_page == "assist_prediction":
+    assist_prediction_page()
+elif st.session_state.selected_page == "match_winner":
+    match_winner_prediction_page()
